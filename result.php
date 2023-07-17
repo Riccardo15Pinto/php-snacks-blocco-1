48c74d553ae79d4c13908142743fa077fe637fbb
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-17 15:08:36 +0200

    check name lenght and charset email

--- result.php
+++ result.php
@@ -4,6 +4,34 @@ $name = $_GET['name'];
 $email = $_GET['email'];
 $age = $_GET['age'];
 
-echo $name;
+// if (strlen($name) >= 3) {
+
+//     echo $name;
+// }
+
+// if (preg_match('/(@?| .?)/', $email)) {
+//     echo 'dajeee';
+// }
+
+
+
 echo $email;
 echo $age;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- <?php if (strlen($name) >= 3) : ?>
+        <h1><?= $name ?></h1>
+    <?php endif ?> -->
+</body>
+
+</html>
